30f8f0f9652e17719668762ba85d3786470aa05d
gourang lawane <117590608+Gourang-pro@users.noreply.github.com>
2023-01-21 13:40:17 +0530

    Add files via upload

---
+++ area of triangle.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int l,b,area;
+    printf("\nenter the value of length : ");
+    scanf("%d",&l);
+
+    printf("\nenter the value of breadth :");
+    scanf("%d",&b);
+
+    area=l*b;
+
+    printf("\nthe area of the rectangle : %d",area);
+
+
+
+
+    return 0;
+
+}
